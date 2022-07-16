1cda18588e9a73d8ec6fb3372552934afb991226
nguyen89mke <nguyen89.mke@gmail.com>
2022-07-15 17:52:42 -0700

    Fixing JS

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const firstEmoji = document.querySelector('closed');
 const secondEmoji = document.querySelector('open');
 
-// Add Event Lister
-
 firstEmoji.addEventListener('click', () => {
     if (firstEmoji.classList.contains.closed) {
-        firstEmoji.classList.remove('active');
         secondEmoji.classList.add('active');
+        firstEmoji.classList.remove('active');
     }
-})
-    
-secondEmoji.addEventListener('click', () => {
-    if (secondEmoji.classList.contains.open) {
-        secondEmoji.classList.remove('active');
-        firstEmoji.classList.add('active');
-    }
-})
+ })
